cbb8ad062624096c46dbd5343f82307028094f02
Faezrah Rizalman <fwrizalman@gmail.com>
2014-07-07 21:29:54 +0100

    Generated post model and display posts on homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
+
+  def create
+    @posts = Post.new(params[:posts].permit(:title, :description))
+    @posts.save
+    redirect_to @posts
+
+    # render plain: params[:posts].inspect
   end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
 describe 'Posts' do
-  it 'displays no posts' do
-    visit '/posts'
-    expect(page).to have_content 'No posts yet'
+  context 'no posts' do
+    it 'displays no posts' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+    end
+
+    it 'prompts to create a new post' do
+      visit '/posts'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'New post'
+    end
+
+    it 'create a post' do
+      visit '/posts'
+      click_link 'New post'
+      expect(current_path).to eq '/posts/new'
+      expect(page).to have_content 'Create a post'
+    end
   end
 
-  it 'prompts to create a new post' do
-    visit '/posts'
-    expect(page).to have_content 'No posts yet'
-    expect(page).to have_link 'New post'
-  end
+  context 'with posts' do
+    before { Post.create(title: 'First post', description: 'My first post') }
 
-  it 'create a post' do
-    visit '/posts'
-    click_link 'New post'
-    expect(current_path).to eq '/posts/new'
-    expect(page).to have_content 'Create a post'
+    it 'displays the posts' do
+      visit '/posts'
+      expect(page).to have_content 'First post'
+    end
   end
 end
