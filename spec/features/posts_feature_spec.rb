f7845994090a69a7629100eea9aaf4364e405511
Faezrah Rizalman <fwrizalman@gmail.com>
2014-07-08 22:54:13 +0100

    Generated user model, added tests to authenticate user before posting

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,37 +1,20 @@
 require 'rails_helper'
 
 describe 'Posts' do
-  context 'with no posts' do
-    it 'displays no posts on homepage' do
-      visit '/posts'
-      expect(page).to have_content 'No posts yet'
-    end
-
-    it 'prompts to create a new post' do
-      visit '/posts'
-      expect(page).to have_content 'No posts yet'
-      expect(page).to have_link 'New post'
-    end
-
-    it 'display a link to create a new post' do
-      visit '/posts'
-      click_link 'New post'
-      expect(current_path).to eq '/posts/new'
-      expect(page).to have_content 'Create a post'
-    end
-
-    it 'adds a new post without an image' do
-      visit '/posts/new'
-      fill_in 'Title', with: 'First post'
-      fill_in 'Description', with: 'My first post'
-      # attach_file 'Image', Rails.root.join('spec/images/bohey_dulang.jpg')
-      click_button 'Post it!'
-      expect(current_path).to eq posts_path
-      expect(page).to have_content 'Errors'
-    end
+  it 'displays no posts on homepage' do
+    visit '/posts'
+    expect(page).to have_content 'No posts yet'
   end
 
-  context 'with images' do
+  it 'prompts to create a new post' do
+    visit '/posts'
+    expect(page).to have_content 'No posts yet'
+    expect(page).to have_link 'New post'
+  end
+end
+
+describe 'Creating posts' do
+  context 'when logged out' do
     before do
       Post.create title: 'First post', description: 'My first post', image: File.new(Rails.root + 'spec/images/bohey_dulang.jpg')
     end
@@ -39,6 +22,30 @@ describe 'Posts' do
     it 'displays the posts on homepage' do
       visit '/posts'
       expect(page).to have_css 'img.uploaded-pic'
+    end
+
+    it 'is impossible' do
+      visit '/posts'
+      click_link 'New post'
+      expect(page).to have_content 'Sign in'
+      expect(page).not_to have_field 'Title'
+    end
+  end
+
+  context 'when logged in' do
+    before do
+      alex = User.create(email: 'alex@alex.com', password: '12345678', password_confirmation: '12345678')
+      login_as alex
+      # Post.create title: 'First post', description: 'My first post', image: File.new(Rails.root + 'spec/images/bohey_dulang.jpg')
+    end
+
+    it 'adds a new post without an image' do
+      visit '/posts/new'
+      fill_in 'Title', with: 'First post'
+      fill_in 'Description', with: 'My first post'
+      click_button 'Post it!'
+      expect(current_path).to eq posts_path
+      expect(page).to have_content 'Errors'
     end
 
     it 'can add a photo to post' do
@@ -49,6 +56,6 @@ describe 'Posts' do
       click_button 'Post it!'
       expect(current_path).to eq posts_path
       expect(page).to have_css 'img.uploaded-pic'
-    end 
+    end
   end
 end
