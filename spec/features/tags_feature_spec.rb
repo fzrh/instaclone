03425d1a3b968abd823aebe0eaec627f84c68948
Faezrah Rizalman <fwrizalman@gmail.com>
2014-07-11 15:17:24 +0100

    Added page to show posts associated with tags

--- spec/features/tags_feature_spec.rb
+++ spec/features/tags_feature_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'tagging posts' do
   context 'when logged in' do
+    let!(:alex) { User.create(email: 'alex@alex.com', password: '12345678', password_confirmation: '12345678') }
     before do
-      alex = User.create(email: 'alex@alex.com', password: '12345678', password_confirmation: '12345678')
       login_as alex
     end
 
@@ -17,6 +17,16 @@ describe 'tagging posts' do
       expect(current_path).to eq posts_path
       expect(page).to have_link '#yolo'
       expect(page).to have_link '#swag'
+    end
+
+    it 'should filter posts by selected tags' do
+      alex.posts.create title: 'First post', tag_names: 'yolo', image: File.new(Rails.root + 'spec/images/bohey_dulang.jpg')
+      alex.posts.create title: 'Second post', tag_names: 'swag', image: File.new(Rails.root + 'spec/images/bohey_dulang.jpg')
+      visit '/posts'
+      click_link 'yolo'
+      expect(page).to have_css 'h1', 'Posts associated with yolo'
+      expect(page).to have_content 'First post'
+      expect(page).not_to have_content 'Second post'
     end
   end
 end
