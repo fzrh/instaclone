65b7f51cd2907a876cfd4c92ded46e56cc2e5d07
Faezrah Rizalman <fwrizalman@gmail.com>
2014-12-05 12:09:36 +0800

    Testing Codeship - secret key base for test environment

--- spec/features/maps_features_spec.rb
+++ spec/features/maps_features_spec.rb
@@ -6,12 +6,11 @@ describe 'maps' do
       alex = User.create email: 'alex@alex.com', password: '12345678', password_confirmation: '12345678'
       login_as alex
       alex.posts.create title: 'First post', description: 'My first post', address: '25 City Road, London', image: File.new(Rails.root + 'spec/images/bohey_dulang.jpg')
-      
+
     end
 
     it 'displays a map', js: true do
       visit '/posts'
-      save_and_open_page  
       click_link 'Map'
       expect(page).to have_map
       # expect(page).to have_css '.gm-style'
